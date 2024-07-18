7342614c9b22fac689f942f5942470786cc5f063
VargasGandolini <avg072023@gmail.com>
2024-07-18 16:50:27 -0400

    Inicio e Imagenes

--- weather/src/App.jsx
+++ weather/src/App.jsx
@@ -1,7 +1,87 @@
-import React from "react";
+import { useEffect, useState } from "react";
+import { localizate, apiDatosCurrent, apiImages } from "./server/apis.js";
+import "./App.css";
+import { TiWeatherPartlySunny } from "react-icons/ti";
+import { FaList } from "react-icons/fa";
+import { CiMap } from "react-icons/ci";
 
 const App = () => {
-  return <div>App</div>;
+  const [localization, setLocalization] = useState(null);
+  const [weatherActual, setWeatherActual] = useState(null);
+  const [imagenes, setImagenes] = useState(null);
+  const [ciudad, setCiudad] = useState("");
+  const [ciudades, setCiudades] = useState([]);
+
+  useEffect(() => {
+    localizate()
+      .then((data) => setLocalization(data))
+      .catch((err) => console.error(err));
+  }, []);
+
+  useEffect(() => {
+    localization
+      ? (apiDatosCurrent(localization.latitude, localization.longitude)
+          .then((data) => setWeatherActual(data))
+          .catch((err) => console.error(err)),
+        apiImages(localization.latitude, localization.longitude))
+          .then((data) => setImagenes(data))
+          .catch((err) => console.error(err))
+      : "";
+  }, [localization]);
+
+  const monstrar = () => {
+    console.log(ciudad);
+  };
+
+  return (
+    <>
+      <header>
+        <input
+          placeholder="Search for cities"
+          value={ciudad}
+          onChange={(e) => setCiudad(e.target.value)}
+        />
+        <aside>
+          <button>
+            <div className="svg">
+              <TiWeatherPartlySunny />
+            </div>
+            Weather
+          </button>
+          <button>
+            <div className="svg">
+              <FaList />
+            </div>
+            Cities
+          </button>
+          <button>
+            <div className="svg">
+              <CiMap />
+            </div>
+            Map
+          </button>
+        </aside>
+      </header>
+      {weatherActual && localization && imagenes ? (
+        <main>
+          <div className="actual">
+            <div className="datos">
+              <h1>{localization.city}</h1>
+              <p>
+                Chance of rain: {Number(weatherActual.current.precip_mm) / 100}%
+              </p>
+              <h1>{Math.trunc(Number(weatherActual.current.temp_c))}°C</h1>
+            </div>
+          </div>
+          <p>{imagenes.current.icon}</p>
+          <p>{imagenes.current.icon_num}</p>
+          <img src={weatherActual.current.condition.icon} />
+        </main>
+      ) : (
+        ""
+      )}
+    </>
+  );
 };
 
 export default App;
--- weather/src/server/apis.js
+++ weather/src/server/apis.js
@@ -5,15 +5,29 @@ export const localizate = async () => {
         const data = await response.json();
         return data;
     } catch (error) {
+        console.error("Error fetching data:", error);
+        throw error;
+    }
+};
+
+const key_weather = '75bfd16d30d2499a99f162613241807'
+
+export const apiDatosCurrent = async (lat, lon) => {
+    const url = `http://api.weatherapi.com/v1/current.json?key=${key_weather}&q=${lat},${lon}&aqi=no`;
+    try {
+        const response = await fetch(url);
+        const data = await response.json();
+        return data;
+    } catch (error) {
         console.error("Error fetching weather data:", error);
         throw error;
     }
 };
 
-const key_weather = ''
+const key_images = 'cwztj7orycmdggexfepvavvjpipiwyffl7t1gupi'
 
-export const api = async () => {
-    const url = ``;
+export const apiImages = async (lat, lon) => {
+    const url = `https://www.meteosource.com/api/v1/free/point?lat=${lat}&lon=${lon}&sections=current%2Chourly&language=en&units=auto&key=${key_images}`;
     try {
         const response = await fetch(url);
         const data = await response.json();
